15cc3ae45dd2ba8ad166d05d0438ac5eb7387ac3
Chris Noxz <chris@noxz.tech>
2020-06-09 09:27:46 +0200

    add brave-browser to client rules

--- config.def.h
+++ config.def.h
@@ -49,9 +49,10 @@ static const Rule rules[] = {
 	 * WM_CLASS(STRING) = instance, class
 	 * WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "tabbed",   "vimb",     NULL,       1 << 1,       0,           -1 },
+	/* class            instance    title   tags mask   isfloating   monitor */
+	{ "Gimp",           NULL,       NULL,   0,          1,           -1 },
+	{ "tabbed",         "vimb",     NULL,   1 << 1,     0,           -1 },
+	{ "Brave-browser",  NULL,       NULL,   1 << 1,     0,           -1 },
 };
 
 /* layout(s) */
