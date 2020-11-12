692ab19d88732811618db19cd5e019800c3c5471
Chris Noxz <chris@noxz.tech>
2020-11-12 14:23:03 +0100

    remove client specific rules

--- config.def.h
+++ config.def.h
@@ -44,15 +44,12 @@ static char colors[][4][8]         = {
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
-static const Rule rules[] = {
+static const Rule rules[] = { NULL
 	/* xprop(1):
 	 * WM_CLASS(STRING) = instance, class
 	 * WM_NAME(STRING) = title
 	 */
 	/* class            instance    title   tags mask   isfloating   monitor */
-	{ "Gimp",           NULL,       NULL,   0,          0,           -1 },
-	{ "tabbed",         "vimb",     NULL,   1 << 1,     0,           -1 },
-	{ "Brave-browser",  NULL,       NULL,   1 << 1,     0,           -1 },
 };
 
 /* layout(s) */
